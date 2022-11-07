d398e03e14f97c44387ed14947eff50001e1be26
Mohácsi László <mohacsi@altinum.hu>
2022-11-07 18:34:39 +0100

    Customers tábla tartalma megjelenik, szűrhető

--- UserControl1.Designer.cs
+++ UserControl1.Designer.cs
@@ -35,15 +35,17 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(11, 17);
+            this.textBox1.Location = new System.Drawing.Point(11, 30);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(204, 31);
+            this.textBox1.Size = new System.Drawing.Size(319, 31);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // listBox1
             // 
@@ -53,7 +55,7 @@
             this.listBox1.ItemHeight = 25;
             this.listBox1.Location = new System.Drawing.Point(11, 67);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(204, 479);
+            this.listBox1.Size = new System.Drawing.Size(319, 479);
             this.listBox1.TabIndex = 1;
             // 
             // listBox2
@@ -62,7 +64,7 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 25;
-            this.listBox2.Location = new System.Drawing.Point(234, 67);
+            this.listBox2.Location = new System.Drawing.Point(336, 67);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(204, 479);
             this.listBox2.TabIndex = 2;
@@ -84,17 +86,17 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(457, 67);
+            this.dataGridView1.Location = new System.Drawing.Point(546, 67);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(509, 479);
+            this.dataGridView1.Size = new System.Drawing.Size(433, 479);
             this.dataGridView1.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(978, 200);
+            this.button1.Location = new System.Drawing.Point(985, 201);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(66, 57);
             this.button1.TabIndex = 5;
@@ -104,17 +106,27 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(978, 263);
+            this.button2.Location = new System.Drawing.Point(985, 264);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(66, 57);
             this.button2.TabIndex = 6;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Customers:";
+            // 
             // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -124,6 +136,7 @@
             this.Controls.Add(this.textBox1);
             this.Name = "UserControl1";
             this.Size = new System.Drawing.Size(1282, 581);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -139,5 +152,6 @@
         private DataGridView dataGridView1;
         private Button button1;
         private Button button2;
+        private Label label1;
     }
 }
--- UserControl1.cs
+++ UserControl1.cs
@@ -12,9 +12,29 @@ namespace BikeStoreMintaZH
 {
     public partial class UserControl1 : UserControl
     {
+        Models.se_bikestoreContext context = new();
         public UserControl1()
         {
             InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            FilterCustomers();
+        }
+
+        private void FilterCustomers()
+        {
+            var customers = from x in context.Customers
+                            where x.Email.Contains(textBox1.Text)
+                            select x;
+            listBox1.DataSource = customers.ToList();
+            listBox1.DisplayMember = "Email";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            FilterCustomers();
         }
     }
 }
